c109301a168d0c2454d04e4fe656c3b1dd389c2a
Hannah Chan <hannah.chan@outlook.com>
2023-04-21 17:29:00 +0900

    Add OpenTelemetry dependency

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -21,10 +21,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+    // api("org.apache.commons:commons-math3:3.6.1")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.1-jre")
+    implementation("io.opentelemetry:opentelemetry-api:1.25.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
