f5f9e886758ab306a41cf170578558c2ec543a64
Hannah Chan <hannah.chan@outlook.com>
2023-04-22 15:11:02 +0900

    Add Counter and Histogram

--- lib/src/main/java/featuregates/internal/Instrumentation.java
+++ lib/src/main/java/featuregates/internal/Instrumentation.java
@@ -1,5 +1,19 @@
 package featuregates.internal;
 
+import io.opentelemetry.api.metrics.DoubleHistogram;
+import io.opentelemetry.api.metrics.LongCounter;
+
 class Instrumentation {
 
+    private static final LongCounter EXECUTION_COUNTER = Library.METER
+            .counterBuilder("feature.gate.executions")
+            .setUnit(null)
+            .setDescription("measures the number of times a feature gate has been executed")
+            .build();
+
+    private static final DoubleHistogram EXECUTION_DURATION_HISTOGRAM = Library.METER
+            .histogramBuilder("feature.gate.duration")
+            .setUnit("ms")
+            .setDescription("measures the duration of feature gate executions")
+            .build();
 }
